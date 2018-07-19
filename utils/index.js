8ff4fdc70b08e9c7a6c488e2a2bb30a019d39433
Layne Faler <laynefaler@gmail.com>
2018-07-19 10:28:06 -0700

    Updates to utils for circular imports

--- utils/index.js
+++ utils/index.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 const index = fs.readdirSync(path.join(__dirname, 'methods')).reduce((acc, item) => {
-
+    acc = { ...acc, ...require(path.join(__dirname, 'methods', item)) };
+    return acc;
 }, {});
 
 module.exports = index;
